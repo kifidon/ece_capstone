548360de3583b9f672f0f854e68b4f380a0c1a21
kifidon <kigidon@ualberta.ca>
2026-03-23 21:28:28 -0600

    Migration

--- backend/api/migrations/0006_remove_edgedevice_type_edgedevice_device_type.py
+++ backend/api/migrations/0006_remove_edgedevice_type_edgedevice_device_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0004_edgeevent_trigger_device'),
+        ('api', '0005_edgeevent_alert_reasoning_alter_edgeevent_hub_device'),
     ]
 
     operations = [
